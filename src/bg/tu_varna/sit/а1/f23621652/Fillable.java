e5ea2108de324a36e959babd026f137f47233fd3
Elitsa Ilarionova <elitza_@mail.bg>
2025-03-05 23:34:02 +0200

    removed old files

--- src/bg/tu_varna/sit/а1/f23621652/Fillable.java
+++
@@ -1,5 +0,0 @@
-package bg.tu_varna.sit.а1.f23621652;
-
-public interface Fillable {
-    //?
-}
--- src/bg/tu_varna/sit/а1/f23621652/SVGObjectException.java
+++
@@ -1,7 +0,0 @@
-package bg.tu_varna.sit.а1.f23621652;
-
-public class SVGObjectException extends Exception{
-    public SVGObjectException(String message) {
-        super(message);
-    }
-}
